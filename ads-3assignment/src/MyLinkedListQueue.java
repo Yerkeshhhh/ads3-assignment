d9b83ac3e7737ffe09d301d0960c6f566ff0108b
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-05-01 22:05:51 +0600

    jkbh

--- ads-3assignment/src/MyLinkedListQueue.java
+++ ads-3assignment/src/MyLinkedListQueue.java
@@ -1,2 +1,27 @@
-public class MyLinkedListQueue {
+public class MyLinkedListQueue<E> implements MyQueue<E> {
+
+    @Override
+    public void enqueue(E element) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
