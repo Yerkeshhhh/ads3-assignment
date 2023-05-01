1eb65af5824d0894eb305a029e004a9180566779
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-05-01 20:20:45 +0600

    jkbh

---
+++ ads-3assignment/src/MyList.java
@@ -0,0 +1,16 @@
+public interface MyList<E> {
+    int size();
+    boolean isEmpty();
+    boolean contains(Object o);
+    boolean add(E e);
+    boolean remove(Object o);
+    void clear();
+    E get(int index);
+    E set(int index, E element);
+    void add(int index, E element);
+    E remove(int index);
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+
+
+}
