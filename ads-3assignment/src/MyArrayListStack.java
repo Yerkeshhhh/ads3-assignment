0480491b31418a6ecfa417118389d243f9bdba86
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-05-01 21:54:07 +0600

    jkbh

--- ads-3assignment/src/MyArrayListStack.java
+++ ads-3assignment/src/MyArrayListStack.java
@@ -1,2 +1,27 @@
-public class MyArrayListStack {
+public class MyArrayListStack<E> implements MyStack<E> {
+
+    @Override
+    public void push(E element) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
