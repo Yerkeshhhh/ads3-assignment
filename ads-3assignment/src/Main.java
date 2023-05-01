1156daf73e71a28b3af22a62d616b2cf06741aaa
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-05-01 22:20:10 +0600

    jkbh

--- ads-3assignment/src/Main.java
+++ ads-3assignment/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyArrayListStack<Integer> array = new MyArrayListStack<>();
+        array.push(1);
+        array.push(2);
+        array.push(3);
+        array.push(4);
+        array.push(5);
+        array.push(7);
+        System.out.println(array.size());
+        System.out.println(array.isEmpty());
+        MyArrayListQueue<Integer> arrayListQueue = new MyArrayListQueue<>();
+        arrayListQueue.enqueue(1);
+        arrayListQueue.enqueue(2);
+        arrayListQueue.enqueue(3);
+
+        MyLinkedListQueue<Integer> myLinkedListQueue = new MyLinkedListQueue<>();
+        myLinkedListQueue.enqueue(2);
+        myLinkedListQueue.enqueue(3);
+        myLinkedListQueue.enqueue(5);
+        myLinkedListQueue.enqueue(6);
+        myLinkedListQueue.enqueue(7);
     }
 }
