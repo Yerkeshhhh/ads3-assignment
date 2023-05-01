e15ed74eb22866b010d746144dda4b18316e3e78
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-05-01 22:19:11 +0600

    jkbh

--- ads-3assignment/src/MyLinkedListStack.java
+++ ads-3assignment/src/MyLinkedListStack.java
@@ -1,2 +1,40 @@
-public class MyLinkedListStack {
+import java.util.NoSuchElementException;
+
+public class MyLinkedListStack<E> implements MyStack<E> {
+
+    private MyLinkedList<E> myLinkedList;
+
+    public MyLinkedListStack(){
+        myLinkedList = new MyLinkedList<>();
+    }
+    @Override
+    public void push(E element) {
+        myLinkedList.add(element);
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return myLinkedList.remove(0);
+    }
+
+    @Override
+    public E peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return myLinkedList.get(0);
+    }
+
+    @Override
+    public int size() {
+        return myLinkedList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return myLinkedList.get(0)==null;
+    }
 }
